bedb26686c57594cf4bcb8a872131690382c808e
Sabīne Liepiņa <sabine.e.liepina@gmail.com>
2023-01-25 20:26:01 +0200

    updated dbConnection file

--- src/db/dbConnection.java
+++ src/db/dbConnection.java
@@ -12,11 +12,11 @@ public class dbConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java34", "root", "Spiegoshana");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM countries");
 
             while (rs.next()){
-                System.out.printf("ID %d username:  %s password: %s full name: %s email: %s \n",
-                        rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));
+                System.out.printf(" Country: %s Capital: %s  \n",
+                        rs.getString(1),rs.getString(2));
             }
             con.close();
 
