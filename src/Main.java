4f7c61625014183683c9481582c1b43f3558c57d
Kristine Necajeva <kristinenecajeva@inbox.lv>
2023-01-26 21:57:00 +0200

    userID

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello ");
         System.out.println("Hello group nr.5");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean i = true;
+
+       System.out.println("Please enter your personal number");
+
+        while(i) {
+            String personalNumber = scanner.nextLine();
+
+            Pattern pattern = Pattern.compile("[0-9]{6}-[0-9]{5}");
+            Matcher matcher = pattern.matcher(personalNumber);
+
+            boolean validPersonalNumber = matcher.matches();
+
+            if (validPersonalNumber != true) {
+                System.out.println("Your inputted personal number is not valid.Enter valid personal number");
+            } else {
+                System.out.println("Please enter username");
+                String username = scanner.nextLine();
+
+            }
+        }
 
     }
 }
