ed05e7834bd7c388d39ccf53c889c7d00552e513
Sabīne Liepiņa <sabine.e.liepina@gmail.com>
2023-01-30 15:38:23 +0200

    fixed the login option with Karlis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 // <<<<<<< HEAD -  these line was imported from git and
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Objects;
 import java.util.Scanner;
 // =======
 import java.util.Scanner;
@@ -18,18 +19,26 @@ public class Main {
         char again = 'y';
 
         try (Connection connection = DriverManager.getConnection(dbURL, username, password)) {
-            //System.out.println("connected to the database");
+
 
             System.out.println("Welcome to our geography quiz!"+ '\n' +"What would you like to do?");
 
             while (again == 'y') {
-                System.out.println("please type 'register' / 'log in' ");
+                System.out.println("Please type 'register' / 'log in' ");
 
                 String action = scanner.nextLine().toLowerCase().trim();
 
+
+                String existingUserName;
                 if (action.equals("register")) {
                     System.out.println("enter a new username");
                     String newUserName = scanner.nextLine();
+
+                    //existingUserName = scanner.nextLine();
+                    if (database.logIn(connection, newUserName)) {
+                        System.out.println("this username is taken");
+                        break;
+                    }
 
                     System.out.println("enter your full name");
                     String newFullName = scanner.nextLine();
@@ -38,8 +47,13 @@ public class Main {
 
                 } else if (action.equals("log in")) {
                     System.out.println("please, input your username");
-                    String existingUserName = scanner.nextLine();
-                    database.logIn(connection, existingUserName);
+                    existingUserName = scanner.nextLine();
+                    if(database.logIn(connection, existingUserName)){
+                        System.out.println("You have logged in");
+                    }else {
+                        System.out.println("Ņot existing user");
+                    }
+
 
                 } else {
                     System.out.println("invalid input");
--- src/db/dbConnection.java
+++ src/db/dbConnection.java
@@ -10,7 +10,7 @@ public class dbConnection {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java34", "root", "1234");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java34", "root", "Spiegoshana");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM countries");
 
